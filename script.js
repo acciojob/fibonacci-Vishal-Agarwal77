17af3a1a03ec60343d74148d03f0ec4077a9fe59
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-11 01:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	if(num===0){
+		console.log(0);
+		return ;
+	}
+	else if(num===1){
+		console.log(1);
+		return ;
+	}
+	let prev=0;
+	let curr=1;
+	let next=0;
+	for(let i=2;i<=num;i++){
+		next=curr+prev;
+		prev=curr;
+		curr=next;
+	}
+	console.log(next);
+	return ;
 }
-
 module.exports = fibonacci;
